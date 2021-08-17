1ef7b2fd159b7fa0c0ae406d5ba84ba923e1c63a
Shaun <shaun@admins-MBP.vaidyans>
2021-08-17 13:03:24 -0500

    ordered by descending hoursInSpace

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 window.addEventListener("load", function(){
-    console.log("You're DOING IT!");
     const url = "https://handlers.education.launchcode.org/static/astronauts.json"
     fetch(url)
     .then(function(response){
       response.json().then(function(json){
         const container = document.getElementById("container");
+        console.log(json)
+        json.sort(function (a,b){
+          return b.hoursInSpace - a.hoursInSpace;
+        });
+        console.log(json)
         for (i = 0; i < json.length; i++){
-            console.log(i);
             let skills = json[i].skills
-            console.log(skills)
+            let hoursInSpace = json[i].hoursInSpace
             container.innerHTML += `<div class="astronaut">
             <div class="bio">
                 <h3>${json[i].firstName} ${json[i].lastName}</h3>
                 <ul>
-                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                <li>Hours in space: ${hoursInSpace}</li>
                 <li>Active: ${json[i].active}</li>
                 <li>Skills: ${skills.join(", ")}</li>
                 </ul>
