63d6f87b276d3a25681134a3ec065697169c6f80
Shaun <shaun@admins-MBP.vaidyans>
2021-08-17 10:54:52 -0500

    concat array output into comma-space separated strings

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ window.addEventListener("load", function(){
         const container = document.getElementById("container");
         for (i = 0; i < json.length; i++){
             console.log(i);
+            let skills = json[i].skills
+            console.log(skills)
             container.innerHTML += `<div class="astronaut">
             <div class="bio">
                 <h3>${json[i].firstName} ${json[i].lastName}</h3>
                 <ul>
                 <li>Hours in space: ${json[i].hoursInSpace}</li>
                 <li>Active: ${json[i].active}</li>
-                <li>Skills: ${json[i].skills}</li>
+                <li>Skills: ${skills.join(", ")}</li>
                 </ul>
             </div>
             <img class="avatar" src="${json[i].picture}">
